cf90bc22579678f35400e8f50c359d41eb4aed9d
Tracey Le <traceyle@outlook.com>
2020-05-28 09:56:24 +1000

    Import XInput4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XboxCtrlrInput;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Rigidbody rigidBody;
+    public XboxController controller;
+
+    public float movementSpeed = 60;
+    public float maxSpeed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+
     }
 
     // Update is called once per frame
@@ -15,4 +28,21 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+    private void MovePlayer()
+    {
+        float axisX = XCI.GetAxis(XboxAxis.LeftStickX, controller);
+        float axisZ = XCI.GetAxis(XboxAxis.LeftStickY, controller);
+
+        Vector3 movement = new Vector3(axisX, 0, axisZ);
+
+        rigidBody.AddForce(movement * movementSpeed);
+
+        // Ensure player can't go faster than the max speed
+        if (rigidBody.velocity.magnitude > maxSpeed)
+        {
+            rigidBody.velocity = rigidBody.velocity.normalized * maxSpeed;
+        }
+    }
+
 }
