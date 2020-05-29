1f3af244d8185477290071144ad77c15d85bfccc
Tracey <traceyle@outlook.com>
2020-05-29 10:19:16 +1000

    Got camera translation working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using XboxCtrlrInput;
 
@@ -12,6 +13,9 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed = 60;
     public float maxSpeed = 5;
 
+    public Vector3 previousRotationDirection = Vector3.forward;
+    public float turnSpeed = 60;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +24,7 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Debug.Log("fixed update");
+        //Debug.Log("fixed update");
         MovePlayer();
         
     }
@@ -28,7 +32,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //MovePlayer();
+        RotatePlayer();
     }
 
     private void MovePlayer()
@@ -37,7 +41,7 @@ public class PlayerController : MonoBehaviour
         float axisZ = XCI.GetAxis(XboxAxis.LeftStickY, controller);
 
         Vector3 movement = new Vector3(axisX, 0, axisZ);
-        Debug.Log(movement*movementSpeed);
+        //Debug.Log(movement*movementSpeed);
 
         rigidBody.AddForce(movement * movementSpeed);
 
@@ -46,6 +50,25 @@ public class PlayerController : MonoBehaviour
         {
             rigidBody.velocity = rigidBody.velocity.normalized * maxSpeed;
         }
+
+
     }
 
+    private void RotatePlayer()
+    {
+        float rotateAxisX = XCI.GetAxis(XboxAxis.RightStickX, controller);
+        float rotateAxisZ = XCI.GetAxis(XboxAxis.RightStickY, controller);
+
+        Vector3 directionVector = new Vector3(rotateAxisX, 0, rotateAxisZ);
+
+        if (directionVector.magnitude < 0.1f)
+        {
+            directionVector = previousRotationDirection;
+        }
+
+        directionVector = directionVector.normalized;
+        previousRotationDirection = directionVector;
+        //rigidBody.AddTorque(directionVector * turnSpeed * Time.deltaTime);
+        rigidBody.AddTorque(directionVector * turnSpeed);
+    }
 }
